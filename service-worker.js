a6bf2b8ecacb1a3ec7c4e5685c503e294db9ca3e
waseemshafi <102962540+waseemshafi@users.noreply.github.com>
2026-06-27 21:25:13 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Khirad's Study Zone - offline service worker (auto-updating) */
-const CACHE = 'khirad-study-zone-v16';
+const CACHE = 'khirad-study-zone-v17';
 const ASSETS = [
   './',
   './index.html',
